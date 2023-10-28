25bf4c72e9a0627ede6911ed275fa9e8dcd47f6c
be3 <d_0711@icloud.com>
2023-10-29 00:36:34 +0900

    add react-router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,66 +1,16 @@
-import { useRef, useState, useCallback } from "react";
-import Webcam from "react-webcam";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
-
-const videoConstraints = {
-  width: 720,
-  height: 360,
-  facingMode: "user",
-};
+import Capture from "./pages/Capture";
 
 function App() {
-  const [isCaptureEnable, setCaptureEnable] = useState<boolean>(false);
-  const webcamRef = useRef<Webcam>(null);
-  const [url, setUrl] = useState<string | null>(null);
-  const capture = useCallback(() => {
-    const imageSrc = webcamRef.current?.getScreenshot();
-    if (imageSrc) {
-      setUrl(imageSrc);
-    }
-  }, [webcamRef]);
-
   return (
     <div className="App">
-      <header>
-        <h1>カメラアプリ</h1>
-      </header>
-      {isCaptureEnable || (
-        <button onClick={() => setCaptureEnable(true)}>開始</button>
-      )}
-      {isCaptureEnable && (
-        <>
-          <div>
-            <button onClick={() => setCaptureEnable(false)}>終了</button>
-          </div>
-          <div>
-            <Webcam
-              audio={false}
-              width={540}
-              height={360}
-              ref={webcamRef}
-              screenshotFormat="image/jpeg"
-              videoConstraints={videoConstraints}
-            />
-          </div>
-          <button onClick={capture}>キャプチャ</button>
-        </>
-      )}
-      {url && (
-        <>
-          <div>
-            <button
-              onClick={() => {
-                setUrl(null);
-              }}
-            >
-              削除
-            </button>
-          </div>
-          <div>
-            <img src={url} alt="Screenshot" />
-          </div>
-        </>
-      )}
+      <BrowserRouter>
+          <Routes>
+            {/* <Route path='/' element={< />}/> */}
+            <Route path='/capture' element={<Capture />}/>
+          </Routes>
+        </BrowserRouter>
     </div>
   );
 }
